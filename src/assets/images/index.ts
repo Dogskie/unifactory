bbaa68300306a827c718345b2394dd27cf6ec1a1
Dogskie <151588914+Dogskie@users.noreply.github.com>
2024-01-07 19:35:44 +0800

    Update index.ts

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -28,7 +28,7 @@ import GUAPX from './guapx.png'
 import SCROLL_ETH from './scroll.io.png'
 import Q from './q.png'
 import ZETA from './Zeta.png'
-import MANTA from './MantaLogo.png'
+
 
 export const CURRENCY = {
   SCROLL_ETH,
