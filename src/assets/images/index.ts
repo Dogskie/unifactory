96cac2c7a83501c99a6247dfe6f58a8308d69e89
Dogskie <151588914+Dogskie@users.noreply.github.com>
2024-01-07 19:12:22 +0800

    Update index.ts

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -28,7 +28,6 @@ import GUAPX from './guapx.png'
 import SCROLL_ETH from './scroll.io.png'
 import Q from './q.png'
 import ZETA from './Zeta.png'
-import MANTA from './manta.png'
 
 
 export const CURRENCY = {
@@ -61,7 +60,6 @@ export const CURRENCY = {
   Q,
   GUAPX,
   ZETA,
-  MANTA_ETH,
 
   // ------------
 
